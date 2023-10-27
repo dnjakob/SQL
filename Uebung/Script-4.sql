2f4078af0cc6332f5743fd2b4c7b3cfe24d945ce
Danjka99 <jakob_daniel@gmx.net>
2023-10-27 11:30:36 +0200

    sync

--- Uebung/Script-4.sql
+++ Uebung/Script-4.sql
@@ -20,3 +20,19 @@ SELECT * FROM cellar_copy2;
 
 -- Aufgabe 4.)
 
+UPDATE cellar_copy2 SET bottles = bottles DIV 2 WHERE producer = 'Buena Vista';
+SELECT * FROM cellar_copy2;
+
+-- Aufgabe 5.)
+
+UPDATE cellar_copy2 SET bottles = bottles - 1 WHERE jahr = 79 OR jahr = 80 OR jahr = 81;
+SELECT * FROM cellar_copy2;
+
+-- Aufgabe 6.)
+
+SELECT bin, wine, producer FROM cellar_copy2 WHERE wine BETWEEN "A" AND "M";
+SELECT bin, wine, producer FROM cellar_copy2 WHERE wine <= 'M';
+
+-- Aufgabe 7.)
+
+SELECT * FROM cellar_copy2 WHERE comments IS NULL OR comments = '';
